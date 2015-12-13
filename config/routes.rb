2740b1b95538ec2722e60cf6512f7106f7273cbe
chrismayu <chrismayu@gmail.com>
2015-12-13 18:52:02 -0500

    remove mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  resources :users
-mount MailPreview => 'mail_preview' if Rails.env.development?end
+  resources :users 
