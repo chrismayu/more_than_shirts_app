30ef0bd426a4df96b8df07e351dbbf9527f037fd
chrismayu <chrismayu@gmail.com>
2015-12-13 18:50:08 -0500

    mail

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,4 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
-mount MailPreview => 'mail_view' if Rails.env.development?end
+mount MailPreview => 'mail_preview' if Rails.env.development?end
